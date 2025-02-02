3b95978ac9d5a461e6dc5dcd3e34637f1a682759
Radman <97783226+radmanon@users.noreply.github.com>
2025-02-02 14:00:22 -0800

    Refactored to be OOP

--- server.js
+++ server.js
@@ -3,59 +3,82 @@ const url = require('url');
 const utils = require('./modules/utils');
 const fileHandler = require('./modules/handler');
 const msg = require('./lang/en').msg;
-
 const fs = require('fs');
 
 
-http.createServer(function(req, res){
-    const q = url.parse(req.url, true);
-    const pathname = q.pathname;
-    const qdata = q.query;
-    const name = qdata.name
+class Server {
+    constructor(port = 8888){
+        this.port = port;
+    }
 
-    if (pathname === '/getDate') {
-        if (!qdata.name || qdata.name.trim() === ''){
-            res.writeHead(400, {'Content-Type': 'text/html'});
-            return res.end(`${msg.noName}`)
+    start(){
+        http.createServer((req,res) => this.handleRequest(req, res)).listen(this.port, () => {
+            console.log(`Server is running at http://localhost:${this.port}/`);
+        });
+    }
+    handleRequest(req,res){
+        const q = url.parse(req.url, true);
+        const pathname = q.pathname;
+        const qdata = q.query;
+        const name = qdata.name
+
+        if (pathname === '/getDate') {
+            return this.handleGetDate(res, qdata);
+        } else if (pathname === '/writeFile') {
+            return this.handleWriteFile(res, qdata);
+        } else if (pathname.startsWith('/readFile/')) {
+            return this.handleReadFile(res, pathname);
+        } else {
+            return this.handleInvalidEndpoint(res);
         }
-    const serverTime = utils.getDate();
-    const responseMsg = `${msg.greeting.replace('%1', name)} Server current date and time is ${serverTime}`
+    }
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(responseMsg);
+    handleGetDate(res, qdata){
+        if (!qdata.name || qdata.name.trim() === '') {
+            res.writeHead(400, { 'Content-Type': 'text/html' });
+            return res.end(`<p style="color:red;">${msg.noName}</p>`);
+        }
+        const serverTime = utils.getDate();
+        const responseMsg = `<p style="color:blue;">${msg.greeting.replace('%1', qdata.name)} Server current date and time is ${serverTime}</p>`;
 
-} else if (pathname === '/writeFile') {
-        if(!qdata.text) {
-            res.writeHead(400, {'Content-Type': 'text/html'});
-            return res.end(`${msg.missingText}`)
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(responseMsg);
+    }
+
+    handleWriteFile(res, qdata){
+        if (!qdata.text || qdata.text.trim() === '') {
+            res.writeHead(400, { 'Content-Type': 'text/html' });
+            return res.end(`<p style="color:red;">${msg.missingText}</p>`);
         }
         fileHandler.appendText('file.txt', qdata.text, (err) => {
-            if (err){
-                res.writeHead(500, {'Content-Type': 'text/html'});
-                return res.end(`${msg.errorWriting}`)
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/html' });
+                return res.end(`<p style="color:red;">${msg.errorWriting}</p>`);
             }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(`${msg.successWriting.replace('%1', qdata.text)}`);
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(`<p style="color:blue;">${msg.successWriting.replace('%1', qdata.text.trim())}</p>`);
         });
-    } else if (pathname.startsWith('/readFile/')){
-        console.log(pathname)
-        const filename = pathname.replace('/readFile/', '').trim() //extract the file name entered by user and trim() because if user enter spaces it will be empty
-        if (filename === '') {
+    }
+
+
+    handleReadFile(res, pathname){
+        const filename = pathname.replace('/readFile/', '').trim(); //extract the file name entered by user and trim() because if user enter spaces it will be empty
+        if (!filename || filename.includes('/') || filename.includes('..')) {
             res.writeHead(400, { 'Content-Type': 'text/html' });
-            return res.end(`${msg.noFileName}`);
+            return res.end(`<p style="color:red;">${msg.noFileName}</p>`);
         }
         fileHandler.readText(filename, (err, data) => {
-            if (err){
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                return res.end(`${msg.fileNotFound.replace('%1', filename)}`);
+            if (err) {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                return res.end(`<p style="color:red;">${msg.fileNotFound.replace('%1', filename)}</p>`);
             }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(`${msg.successReading.replace('%1', data)}`);
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(`<p style="color:green;">${msg.successReading.replace('%1', data)}</p>`);
         });
-    } else { //if we enter diffrent pathname in url
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end(`${msg.invalidEndpoint}`);
     }
-}).listen(8888, () => {
-    console.log('Server is running at http://localhost:8888/')
-});
+
+    handleInvalidEndpoint(res){
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end(`<p style="color:red;">${msg.invalidEndpoint}</p>`);
+    }
+}
